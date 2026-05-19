bed6df55a4410476e3318f51e68b82879e6bf2c9
AnshBharti <bhartiansh2007@gmail.com>
2026-05-19 13:39:36 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        small = "".join(char.lower() for char in s if char.isalnum())
+        reverse_small = small[::-1]
+        if small != reverse_small:
+            return False
+        return True
